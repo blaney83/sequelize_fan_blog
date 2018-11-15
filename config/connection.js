773ae3ec8f9a53e66e6fa957cfb90970854ea705
Blaney83 <professionallaney@gmail.com>
2018-11-15 00:15:22 -0700

    Saving work.

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,41 @@
 
-var mysql = require("mysql");
+var Sequelize = require("sequelize");
 
-
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root",
-        database: "fan_theories_db"
-    })
-}
-
-    
-connection.connect(function(err){
-    if(err){
-        console.error("error connection: " + err.stack);
-        return;
-    };
-    console.log("connected as id " + connection.threadId);
+var sequelizeConnection = new Sequelize("sequelize_fan_blog", "root", "root", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
 });
 
-module.exports = connection;
+module.exports = sequelizeConnection;
+
+// var mysql = require("mysql");
+
+
+// if(process.env.JAWSDB_URL){
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }else{
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "root",
+//         database: "fan_theories_db"
+//     })
+// }
+
+
+// connection.connect(function(err){
+//     if(err){
+//         console.error("error connection: " + err.stack);
+//         return;
+//     };
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
--- models/theories.js
+++ models/theories.js
@@ -1,31 +1,47 @@
-const orm = require("../config/orm.js");
 
-let theories = {
-    all: function (cb) {
-        orm.all("theories", (res) => {
-            cb(res);
-        })
-    },
-    create: function (cols, vals, cb) {
-        orm.create("theories", cols, vals, (res) => {
-            cb(res);
-        })
-    },
-    update: function (objColVals, condition, cb) {
-        orm.update("theories", objColVals, condition, (res) => {
-            cb(res);
-        })
-    },
-    delete: function (condition, cb) {
-        orm.delete("theories", condition, (res) => {
-            cb(res);
-        })
-    },
-    readName: function (condition, cb) {
-        orm.readName("theories", condition, (res) => {
-            cb(res);
-        })
-    }
-}
+var sequelizeConnection = require("../config/connection.js")
 
-module.exports = theories;
+var theoryModel = sequelizeConnection.define("theory", {
+    id: Sequelize.INT,
+    media_name: Sequelize.STRING,
+    creator: Sequelize.STRING,
+    date_posted: Sequelize.DATE,
+    theory: Sequelize.STRING,
+    likes: Sequelize.INT,
+})
+
+theoryModel.sync();
+
+module.exports = theoryModel;
+
+// const orm = require("../config/orm.js");
+
+// let theories = {
+//     all: function (cb) {
+//         orm.all("theories", (res) => {
+//             cb(res);
+//         })
+//     },
+//     create: function (cols, vals, cb) {
+//         orm.create("theories", cols, vals, (res) => {
+//             cb(res);
+//         })
+//     },
+//     update: function (objColVals, condition, cb) {
+//         orm.update("theories", objColVals, condition, (res) => {
+//             cb(res);
+//         })
+//     },
+//     delete: function (condition, cb) {
+//         orm.delete("theories", condition, (res) => {
+//             cb(res);
+//         })
+//     },
+//     readName: function (condition, cb) {
+//         orm.readName("theories", condition, (res) => {
+//             cb(res);
+//         })
+//     }
+// }
+
+// module.exports = theories;
